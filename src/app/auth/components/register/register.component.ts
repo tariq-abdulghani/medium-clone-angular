3aaa5938d2535a43660c5b51375c98e8935c484b
tariq-abdulghani <tariq.abdulghani.hussein@gmail.com>
2024-11-18 16:27:01 +0200

    register layout

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,11 +1,26 @@
 import { Component } from "@angular/core";
+import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
 
 
 @Component({
     selector: 'mc-register',
     standalone: true,
+    imports:[ReactiveFormsModule],
     templateUrl: './register.component.html'
 })
 export class RegisterComponent {
     
+    form = this.fb.nonNullable.group({
+        userName: ['', Validators.required],
+        email: ['', Validators.required],
+        password: ['', Validators.required]
+    });
+    
+    constructor(private fb: FormBuilder) {
+        
+    }
+
+    onSubmit(){
+        console.log('form ', this.form.getRawValue())
+    }
 }
