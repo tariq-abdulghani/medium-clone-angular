5326d0d453223b348cddb3269db8301841db4e59
tariq-abdulghani <tariq.abdulghani.hussein@gmail.com>
2024-11-18 02:45:07 +0200

    adding register feature

---
+++ src/app/auth/components/register/register.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'mc-register',
+    standalone: true,
+    templateUrl: './register.component.html'
+})
+export class RegisterComponent {
+    
+}
